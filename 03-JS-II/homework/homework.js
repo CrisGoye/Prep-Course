c936d10d792c2f256d39976320b7bae43640e39d
CrisGoye <markaymarka.publicidad@hotmail.com>
2022-02-23 00:41:54 -0500

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -47,17 +47,18 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-} if ("idioma" === "aleman") {
-  return "Guten Tag!";
-} if ("idioma" === "mandarin") {
-  return "Ni Hao!";
-} if ("idioma" === "ingles") {
-  return "Hello!";
-} if ("idioma" != "aleman" || "mandarin" || "ingles " || "undefined") {
- return "Hola!";
+  if (idioma === "aleman") {
+    return "Guten Tag!";
+  } else if (idioma === "mandarin") {
+    return "Ni Hao!";
+  } else if (idioma === "ingles") {
+    return "Hello!";
+  } else if ("idioma" === "aleman" || "mandarin" || "ingles " || "undefined") {
+    return "Hola!";
+  }
 }
 
- 
+
 function colors(color) {
   //La función recibe un color. Devolver el string correspondiente:
   //En caso que el color recibido sea "blue", devuleve --> "This is blue"
@@ -66,12 +67,24 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-} 
+  switch (color) {
+    case "blue":
+      return ("This is blue")
+    case "red":
+      return ("This is red")
+    case "green":
+      return ("This is green")
+    case "orange":
+      return ("This is orange")
+    default: return ("Color not found")
+  }
+}
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  
 }
 
 function estaEnRango(numero) {
